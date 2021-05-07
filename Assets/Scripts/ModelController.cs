f5ed690509d474c7331538c81bf724d71b5f2ca5
89trillion-Zhaochi <zhaochi@89trillion.com>
2021-05-07 17:19:32 +0800

    init is final
    Good luck!

---
+++ Assets/Scripts/ModelController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+public class ModelController : MonoBehaviour
+{
+    [SerializeField] private Animator infantryAnim;
+    private static readonly int IsAttack = Animator.StringToHash("isAttack");
+    private static readonly int IsIdle = Animator.StringToHash("isIdle");
+    private static readonly int IsRun = Animator.StringToHash("isRun");
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A)) infantryAnim.SetTrigger(IsAttack);//按A时放攻击动画
+        else if(Input.GetKeyDown(KeyCode.I)) infantryAnim.SetTrigger(IsIdle);//按I时放站立动画
+        else if(Input.GetKeyDown(KeyCode.R)) infantryAnim.SetTrigger(IsRun);//按R时放奔跑动画
+        
+    }
+}
